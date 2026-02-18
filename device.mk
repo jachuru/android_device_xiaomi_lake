17c5336ac33baebf2b332dd9f85b737719145ffa
jachuru <rokaiceroka@gmail.com>
2026-02-18 23:29:44 +0800

    Update device.mk with init_boot postinstall configuration

--- device.mk
+++ device.mk
@@ -11,7 +11,9 @@ AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+    POSTINSTALL_OPTIONAL_system=true \
+    RUN_POSTINSTALL_init_boot=false \
+    POSTINSTALL_OPTIONAL_init_boot=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
