a09a0e184fbe80ddd19edf9178b35c80294b5259
jachuru <rokaiceroka@gmail.com>
2026-02-19 20:51:02 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,58 @@
 # TWRP Configuration
 
-# Include encryption support
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+# 2nd architecture
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_64_BIT_BINDER := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 \
+    androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x07c08000
+BOARD_TAGS_OFFSET := 0x0bc08000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# Encryption
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_FBE := true
 TW_INCLUDE_FBE_METADATA_ENCRYPTION := true
 BOARD_USES_METADATA_PARTITION := true
+
+# A/B Partitions
+AB_OTA_PARTITIONS += boot
+AB_OTA_PARTITIONS += vendor_boot
+AB_OTA_PARTITIONS += init_boot
+AB_OTA_PARTITIONS += system
+AB_OTA_PARTITIONS += system_ext
+AB_OTA_PARTITIONS += product
+AB_OTA_PARTITIONS += vendor
+AB_OTA_PARTITIONS += odm
+AB_OTA_PARTITIONS += mi_ext
+
+# Kernel modules
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+BOARD_VENDOR_KERNEL_MODULES :=
+
+# Dynamic partitions
+BOARD_SUPER_PARTITION_SIZE := 4563402752
+BOARD_SUPER_PARTITION_GROUPS := main
+BOARD_MAIN_SIZE := 4563402752
+BOARD_MAIN_PARTITION_LIST := system vendor product system_ext odm mi_ext vendor_dlkm odm_dlkm system_dlkm
